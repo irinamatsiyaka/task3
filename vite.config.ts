0bd750baacc82ee4fb4aa612d468e8cf6863b69b
Irina Matsiyaka <irinamaciaka@gmail.com>
2025-11-12 23:10:01 +0300

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), TanStackRouterVite(), tailwindcss()],
+   base: "/task3/",
    test: {
       environment: "jsdom",
       globals: true,
